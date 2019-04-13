b957e54e61c7c1ce8bbcd6dd346e2ef059d7c522
Shane Lee <shaneleephoto@gmail.com>
2019-04-13 06:57:45 +0100

    Add files via upload

---
+++ __listf_files.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
+
+
+def main():
+
+    dirName = '/Volumes/HDD/Books'
+
+    # Get the list of all files in directory tree at given path
+    listOfFiles = getListOfFiles(dirName)
+
+    # Print the files
+    for elem in listOfFiles:
+        print(elem)
+
+    print ("****************")
+
+    # Get the list of all files in directory tree at given path
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += filenames
+
+
+    # Print the files
+    for elem in listOfFiles:
+        print(elem)
+
+    df = pd.DataFrame(listOfFiles, columns=["File"], )
+    df.to_csv('__BOOKS.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
